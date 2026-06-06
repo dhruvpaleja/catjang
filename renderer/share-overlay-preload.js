4ecff893d9a0c53102a26d8aa19dff5ac2617b1e
jandev-png <291155296+jandev-png@users.noreply.github.com>
2026-06-06 22:18:03 +0200

    Remove project following DMCA takedown; point to janpurr

--- renderer/share-overlay-preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("overlayAPI", {
-  onInit: (cb) => ipcRenderer.once("share-overlay-init", (_e, data) => cb(data)),
-  onCropUpdate: (cb) => ipcRenderer.on("share-crop-update", (_e, crop) => cb(crop)),
-});
